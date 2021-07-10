8fff6a7ec20e7fb18bbecdf03078a633fa45ff2e
vikoluna <vmlm2509@gmail.com>
2021-07-10 15:33:20 -0500

    Basics of R

---
+++ RBasics.R
@@ -0,0 +1,44 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: vikoluna
+# Created on: 7/10/21
+# ---------------------------------------
+
+# This is a coment
+
+print("Hello World") # Print Text
+print("2+2 = ")
+2+2 # Gives the value of the sum
+# ---------------------------------------
+
+x <- 2 # Saves a variable
+print("x value")
+x # Print the value of the variable
+print("x value")
+x <- 3
+x
+# ---------------------------------------
+
+x * 2 + 2 - 2 + 4 # Makes operations with the var
+x # shows the new value of the variable
+# ---------------------------------------
+
+y <- c(1, 2, 3, 4, 5) # Array of numbers containig a set of numbers from 1-5
+y
+# ---------------------------------------
+
+y <- 1:10 # Fill values 1-10
+y
+# ---------------------------------------
+
+x <- y <- 1:10 # Changes variables
+x # showing the new values
+y
+# ---------------------------------------
+
+x + y
+z <- x+y # savin two vars in a new one
+z2 <- x*y
+X <- 10
+X
+# ---------------------------------------
---
+++ main.R
@@ -0,0 +1,5 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: vikoluna
+# Created on: 7/10/21
+
